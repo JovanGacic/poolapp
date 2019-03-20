ecdeadaf5e1f2b5758cf70743b411dc86b604007
JovanGacic <jovangacic@gmail.com>
2019-03-20 22:37:50 +0100

    new registration

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,13 +1,11 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import LoginForm from './src/components/LoginForm';
-import Registration from './src/components/Registration';
 
 const AppNavigator = createStackNavigator({
     Login: { screen: LoginForm,
             navigationOptions: { header: null }
     },
-    Registration: { screen: Registration
-    },
+    
 },
     {
         initialRouteName: 'Login'
